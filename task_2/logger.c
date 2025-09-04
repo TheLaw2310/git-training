fda022e0fd217b22c7919f433789c24c19c63d9e
[Mohamad Al Kary] <[mhdalkary@gmail.com]>
2025-09-03 23:09:38 -0400

    refactor(logger): cleaned up the code and got rid of unnecissary memory allocations

--- task_2/logger.c
+++ task_2/logger.c
@@ -1,5 +1,20 @@
 #include "logger.h"
 
+void log_info(const char* message) {
+    if(message == NULL) {return;}
+    printf("[INFO] %s\n", message);
+}
+
+void log_warn(const char* message) {
+    if(message == NULL) {return;}
+    printf("[WARN] %s\n", message);
+}
+
+void log_error(const char* message) {
+    if(message == NULL) {return;}
+    printf("[ERROR] %s\n", message);
+}
+
 int main(){
     log_info("App started");
     log_warn("Low memory");
